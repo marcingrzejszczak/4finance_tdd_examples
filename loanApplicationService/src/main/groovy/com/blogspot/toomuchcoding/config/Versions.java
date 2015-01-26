cc972b6a6edc6dd0d05da07bd7b3703554d6ddcc
Marcin Grzejszczak <marcin@grzejszczak.pl>
2015-01-26 22:41:08 +0100

    Added stubs for fraud

--- loanApplicationService/src/main/groovy/com/blogspot/toomuchcoding/config/Versions.java
+++ loanApplicationService/src/main/groovy/com/blogspot/toomuchcoding/config/Versions.java
@@ -2,7 +2,7 @@ package com.blogspot.toomuchcoding.config;
 
 public final class Versions {
     private Versions() {
-        throw new UnsupportedOperationException("Can't instantiate a utility class");
+        throw new UnsupportedOperationException("Can't instantiate an utility class");
     }
 
     private static final String APP_NAME = "loanApplicationService";
@@ -11,7 +11,4 @@ public final class Versions {
 
     private static final String VERSION_1 = "v1";
     public static final String LOAN_APPLICATION_SERVICE_JSON_VERSION_1 = VND_PREFIX + "." + APP_NAME + "." + VERSION_1 + JSON_TYPE_SUFFIX;
-
-    private static final String FRAUD_APP_NAME = "fraudDetectionService";
-    public static final String FRAUD_SERVICE_JSON_VERSION_1 = VND_PREFIX + "." + FRAUD_APP_NAME + "." + VERSION_1 + JSON_TYPE_SUFFIX;
 }
